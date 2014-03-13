1b04e92a237018af67ed55226ecc962b8202abc4
Volker Benders <Volker.Benders@medicalcolumbus.de>
2014-03-13 20:31:35 +0100

    initial commit

---
+++ src/main/java/com/vbenders/jee7/entity/ExampleEntity.java
@@ -0,0 +1,33 @@
+package com.vbenders.jee7.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by volker on 07.03.14.
+ */
+@Entity
+public class ExampleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
