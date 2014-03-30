caff275fd261ed7914ffb6952e6cb5c80c36d006
Volker M. Benders <vbenders@gmail.com>
2014-03-30 23:35:24 +0200

    Feature:implement jax-rs file upload - added dependency: jax-rs multipar - started upload method

--- src/main/java/com/vbenders/jee7/entity/com/vbenders/jee7/jaxrs/MyResource.java
+++ src/main/java/com/vbenders/jee7/entity/com/vbenders/jee7/jaxrs/MyResource.java
@@ -1,6 +1,10 @@
 package com.vbenders.jee7.entity.com.vbenders.jee7.jaxrs;
 
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 /**
@@ -13,6 +17,14 @@ public class MyResource {
     @GET
     public void echo(){
         System.err.println(">>>> Hello!");
+    }
+
+    @Path("/upload")
+    @POST
+    @Consumes("multipart/form-data")
+    public void uploadFile(MultipartFormDataInput input){
+        System.err.println(">>>> sit back - starting file upload...");
+
     }
 
 }
