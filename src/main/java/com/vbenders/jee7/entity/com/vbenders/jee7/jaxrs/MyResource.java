1dde2b060ecf8d6e9e33aae5f5dd207e7a8f49f7
Volker M. Benders <vbenders@gmail.com>
2014-03-31 00:43:52 +0200

    Feature:implement jax-rs file upload - uploading seems to work ok

--- src/main/java/com/vbenders/jee7/entity/com/vbenders/jee7/jaxrs/MyResource.java
+++ src/main/java/com/vbenders/jee7/entity/com/vbenders/jee7/jaxrs/MyResource.java
@@ -1,19 +1,30 @@
 package com.vbenders.jee7.entity.com.vbenders.jee7.jaxrs;
 
+import org.apache.commons.io.IOUtils;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by mupfel on 30.03.14.
  */
 @Path("/res")
 public class MyResource {
+    public static final String UPLOADED_FILE_PARAMETER_NAME = "file";
     private String data;
-
+    private static  final Logger LOGGER = LoggerFactory.getLogger(MyResource.class);
     @GET
     public void echo(){
         System.err.println(">>>> Hello!");
@@ -22,9 +33,46 @@ public class MyResource {
     @Path("/upload")
     @POST
     @Consumes("multipart/form-data")
-    public void uploadFile(MultipartFormDataInput input){
-        System.err.println(">>>> sit back - starting file upload...");
+    public Response uploadFile(MultipartFormDataInput input) {
+        LOGGER.info(">>>> sit back - starting file upload...");
 
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+        List<InputPart> inputParts = uploadForm.get(UPLOADED_FILE_PARAMETER_NAME);
+
+        for (InputPart inputPart : inputParts){
+
+//            MultivaluedMap<String, String>
+            MultivaluedMap<String, String> headers = inputPart.getHeaders();
+            String filename = getFileName(headers);
+             LOGGER.info(">>>> upload filename " + filename);
+//convert the uploaded file to inputstream
+            try{
+                InputStream inputStream = inputPart.getBody(InputStream.class,null);
+
+                byte [] bytes = IOUtils.toByteArray(inputStream);
+
+                LOGGER.info(">>> File '{}' has been read, size: #{} bytes", filename, bytes.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    private String getFileName(MultivaluedMap<String, String> headers) {
+        String[] contentDisposition = headers.getFirst("Content-Disposition").split(";");
+
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("filename"))) {
+
+                String[] name = filename.split("=");
+
+                String finalFileName = name[1].trim().replaceAll("\"", "");
+                return finalFileName;
+            }
+        }
+        return "unknown";
     }
 
 }
