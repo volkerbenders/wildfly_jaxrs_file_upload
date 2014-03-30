970f74c7a6f6a13e64d9c664fd6126c7241df018
Volker M. Benders <vbenders@gmail.com>
2014-03-31 00:58:34 +0200

    Feature:implement jax-rs file upload - uploading file and writing same file to tmp dir works ok (diff of files is fine)

--- src/main/java/com/vbenders/jee7/entity/com/vbenders/jee7/jaxrs/MyResource.java
+++ src/main/java/com/vbenders/jee7/entity/com/vbenders/jee7/jaxrs/MyResource.java
@@ -5,6 +5,7 @@ import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.rmi.runtime.Log;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -12,6 +13,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -46,18 +49,40 @@ public class MyResource {
             String filename = getFileName(headers);
              LOGGER.info(">>>> upload filename " + filename);
 //convert the uploaded file to inputstream
+
             try{
                 InputStream inputStream = inputPart.getBody(InputStream.class,null);
+
 
                 byte [] bytes = IOUtils.toByteArray(inputStream);
 
                 LOGGER.info(">>> File '{}' has been read, size: #{} bytes", filename, bytes.length);
+                LOGGER.info("rwading ok, lets write..");
+                writeFile(bytes, "/tmp/" + filename);
+
             } catch (IOException e) {
                 e.printStackTrace();
                 return Response.status(Response.Status.BAD_REQUEST).build();
             }
+
         }
         return Response.status(Response.Status.OK).build();
+    }
+
+    private void writeFile(byte[] content, String filename) throws IOException {
+        LOGGER.info(">>> writing #{} bytes to: {}", content.length, filename);
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileOutputStream fop = new FileOutputStream(file);
+
+        fop.write(content);
+        fop.flush();
+        fop.close();
+        LOGGER.info(">>> writing complete: {}", filename);
     }
 
     private String getFileName(MultivaluedMap<String, String> headers) {
